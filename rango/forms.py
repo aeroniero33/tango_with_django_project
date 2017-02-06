04f4a9b64bca8ec09106d539cb5c8d1437a49f2a
aeroniero33 <justthisthing@gmail.com>
2017-02-06 20:29:01 +0000

    Added a base template and page_add functionality

--- rango/forms.py
+++ rango/forms.py
@@ -16,10 +16,6 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-    class Meta:
-        model = Page
-        exclude = ('category',)
-
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
@@ -29,3 +25,7 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
             return cleaned_data
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
